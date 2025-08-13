3917185f43525892b1bffeb6c7d1e158bbc97375
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-08-13 15:48:40 -0500

    Operaciones matemáticas avanzadas en Python: módulo, potencia y más

---
+++ clase8.py
@@ -0,0 +1,27 @@
+#Operadores Numericos
+a = 10
+b = 3
+print("Suma:" , a + b)
+print("Resta:" , a - b)
+print("Multiplicacion:" , a * b)
+print("Potenciación:" , a ** b)
+print("Division:" , a / b)
+print("Parte entera de la division:" , a // b)
+print("Modulo: " , a % b)
+
+a /= 2
+print(a)
+
+operation_1 = 2 + 3 * 4
+operation_2 = (2 + 3) * 4
+print(operation_1)
+print(operation_2)
+operation_3 = (2+3) * (4**2)/8 - 1
+print(operation_3)
+
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(a == b)
+print(a != b)
