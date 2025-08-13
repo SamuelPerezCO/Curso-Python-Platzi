15c653ac182ab974e66df880c5c31c081cb7f95a
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-08-13 09:42:08 -0500

    comentario clase 3 y clase 5 Manejo de Cadenas y Operaciones Básicas en Python

--- clase3.py
+++ clase3.py
@@ -1,2 +1,6 @@
-print("Hola")
-print("Mundo")
+#Clase sobre las variables y la importancia de la identacion (Todo esto es mas un repaso para mi)
+saludo_1 = "Hola"
+saludo_2 = "Hi"
+nombre = "Samuel"
+print(saludo_1)
+print(nombre)
---
+++ clase5.py
@@ -0,0 +1,10 @@
+#Manejo de Cadenas y Operaciones Básicas en Python
+name = 'SAMUEL'
+last_name = ' Pérez Serna '
+print(5 * name)
+print(name + ' '+ last_name)
+print(len(name))
+print(len(last_name))
+print(name.lower())
+print(name.upper())
+print(last_name.strip())
