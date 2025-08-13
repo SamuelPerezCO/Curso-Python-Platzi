288e84483c7f91bdf993c97ccfa67599b6dd4e67
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-08-13 17:14:44 -0500

    Manejo de Matrices y Tuplas en Python

---
+++ clase12.py
@@ -0,0 +1,11 @@
+#Manejo de Matrices y Tuplas en Python
+matrix = [[1,2,3],
+          [4,5,6],
+          [7,8,9]]
+print(matrix[2][1])
+numbers = (1,2,3,4,5)
+print(numbers)
+print(type(numbers))
+print(numbers[0])
+# numbers[0] = 'unos'
+# print(numbers)
